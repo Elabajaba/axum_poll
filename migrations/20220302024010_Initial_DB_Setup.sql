cf447f48db38dcb2ae9d8d2866d077e2b23d3ae6
Elabajaba <Elabajaba@users.noreply.github.com>
2022-03-02 03:07:09 -0500

    initial sqlx migration

---
+++ migrations/20220302024010_Initial_DB_Setup.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS polls
+(
+    poll_id          BLOB PRIMARY KEY    NOT NULL,
+    title       TEXT                NOT NULL,
+    multi       BOOLEAN             NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS poll_options
+(
+    poll_option_id          BLOB PRIMARY KEY    NOT NULL,
+    poll_id     BLOB                NOT NULL,
+    option      TEXT                NOT NULL,
+    votes       INT                 NOT NULL    DEFAULT 0,
+    constraint fk_polls foreign key(poll_id) references polls(poll_id)
+);
